cb42114617e348626e7fe1e3500e619a8c9df70b
theuerse <theuers@posteo.de>
2018-06-30 13:13:33 +0200

    added b-ranking of Tasks

--- MOHEFT/src/MOHEFT.java
+++ MOHEFT/src/MOHEFT.java
@@ -5,15 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class MOHEFT {
     int N, K;
-    String[] instanceTypes;
+    String[] instanceTypes = null;
     HashMap<String, Double> makeSpanMap = new HashMap<String, Double>();
     HashMap<String, Double> costMap = new HashMap<String, Double>();
+
+    String[] dag;
+    String[] bRankedTasks;
 
     public MOHEFT(String configFilePath){
         initFromConfigFile(configFilePath);
@@ -35,6 +36,19 @@ public class MOHEFT {
         makeSpanMap = readMatrixFile(props.getProperty("MAKESPAN_FILE"));
         costMap = readMatrixFile(props.getProperty("COST_FILE"));
 
+        // read dag from file (adjacency list)
+        try {
+            List<String> list = Files.readAllLines(new File(props.getProperty("DAG_FILE")).toPath(), Charset.defaultCharset());
+            list.remove(0); // dump header
+            dag = list.toArray(new String[0]);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        }
+        bRankedTasks = getBRankedTasks();
+
+        // start calculation of schedules
+        System.out.println("B-ranked task order: " + Arrays.toString(bRankedTasks));
     }
 
     private HashMap<String,Double> readMatrixFile(String path){
@@ -44,7 +58,17 @@ public class MOHEFT {
             List<String> list = Files.readAllLines(new File(path).toPath(), Charset.defaultCharset());
             String line = list.get(0); // read header
             String[] parts = line.replace("task,","").split(",");
-            String[] instanceTypes = parts;
+            String[] iTypes = parts;
+
+            // integrity-check
+            if(instanceTypes == null){
+                instanceTypes = iTypes;
+            }else{
+                if(! Arrays.equals(instanceTypes, iTypes)){
+                    throw new IllegalArgumentException("instanceTypes from configFiles do not match");
+                }
+            }
+
             String task = "";
 
             for(int i = 1; i < list.size(); i++){
@@ -53,7 +77,7 @@ public class MOHEFT {
                 task = parts[0];
 
                 for(int j = 1; j < parts.length; j++){
-                    resultMap.put(task+"@"+instanceTypes[j-1],Double.parseDouble(parts[j]));
+                    resultMap.put(task+"@"+iTypes[j-1],Double.parseDouble(parts[j]));
                 }
             }
 
@@ -63,6 +87,69 @@ public class MOHEFT {
         }
 
         return resultMap;
+    }
+
+    private String[] getBRankedTasks(){
+        ArrayList<String> predecessors;
+        ArrayList<String> resultList = new ArrayList<>();
+
+        // find node to begin with
+        predecessors = getPredecessors("end");
+
+        if(predecessors.size() != 1){
+            throw new IllegalArgumentException("DAG has to have one and only one end-node!");
+        }
+
+        HashMap<String,Double> timeOffSetMap = getTime(predecessors.get(0), new HashMap<String,Double>());
+        ValueComparator bvc = new ValueComparator(timeOffSetMap);
+        TreeMap<String, Double> sorted_map = new TreeMap<String, Double>(bvc);
+        sorted_map.putAll(timeOffSetMap);
+
+        return sorted_map.keySet().toArray(new String[0]);
+    }
+
+    private HashMap<String,Double> getTime(String node, HashMap<String,Double> map){
+        double ownTime = getTaskLength(node);
+        double maxPredecessorTime = 0;
+
+        for(String predecessor : getPredecessors(node)){
+            map = getTime(predecessor, map);
+            maxPredecessorTime = Math.max(maxPredecessorTime, map.get(predecessor));
+        }
+
+        map.put(node, ownTime + maxPredecessorTime);
+        return map;
+    }
+
+    // avg of makespans as a concession to our data-model (actual measurements)
+    private double getTaskLength(String task){
+        double makespanSum=0, count=0;
+        for(String instance : instanceTypes){
+            makespanSum+=makeSpanMap.get(task + "@" + instance);
+            count++;
+        }
+        return makespanSum/count;
+    }
+
+    private ArrayList<String> getPredecessors(String vertice){
+        ArrayList<String> predecessors = new ArrayList<>();
+        String[] parts;
+        String nd;
+
+        for(String adjacency : dag){
+            parts = adjacency.split(":");
+            nd = parts[0];
+
+            parts = parts[1].split(",");
+            for(String successor : parts) {
+                if (successor.equals(vertice)) {
+                    predecessors.add(nd);
+                    break;
+                }
+            }
+        }
+
+        return predecessors;
     }
 
 
---
+++ MOHEFT/src/ValueComparator.java
@@ -0,0 +1,19 @@
+// https://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values
+import java.util.Comparator;
+import java.util.Map;
+
+class ValueComparator implements Comparator<String> {
+    Map<String, Double> base;
+
+    public ValueComparator(Map<String, Double> base) {
+        this.base = base;
+    }
+
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return 1;
+        } else {
+            return -1;
+        } // returning 0 would merge keys
+    }
+}
